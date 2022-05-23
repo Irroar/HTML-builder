fa8cf16581697370c5ba89ad3efd5a89fc493e1f
Iryna Lytvynenko <frau4145@gmail.com>
2022-05-23 16:33:12 +0300

    feat: add solution for fourth task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,21 @@
+const fsPromises = require('fs/promises');
+const path = require('path');
 
+const pathToDirectory = path.join(__dirname, 'files');
+
+async function copyDir(pathToDir) {
+  const pathForCopy = pathToDir + '-copy';
+  fsPromises.mkdir(pathForCopy, {recursive: true});
+  const files = await fsPromises.readdir(pathToDir);
+  const copyFiles = await fsPromises.readdir(pathForCopy);
+  for (let file of files) {
+    await fsPromises.copyFile(path.join(pathToDir, file), path.join(pathForCopy, file));
+  }
+
+  for (let copyFile of copyFiles) {
+    if (!files.includes(copyFile)) { await fsPromises.rm(path.join(pathForCopy, copyFile)); }
+  }
+  
+}
+
+copyDir(pathToDirectory);
